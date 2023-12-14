5347b74796110c5a4a22d05262e029121db25f53
chigoox <dikeemmanuel54@gmail.com>
2023-12-14 11:46:04 -0500

    update

--- app/Support/Componets/NavBar/NavBar.jsx
+++ app/Support/Componets/NavBar/NavBar.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 function NavBar() {
     const navMenu = ['Booking', 'Blog']
     return (
-        <div className='bg-[#0d1821] sticky between z-[99999] px-10 top-0 w-full h-14 mb-2'>
+        <div className='bg-[#0d1821] sticky between z-[99999] px-10 top-0 w-full h-14'>
             <div className='border-4 rounded-full w-14 h-14 center border-dotted'>
                 <h1 className="logoTitle text-white font-extrabold">TheHub</h1>
             </div>
             <div className=' evenly text-white gap-4'>
                 {navMenu.map(menuItem => (<div className='hover:text-[#28587b] hover:font-extrabold font-bold '>
-                    <Link href={menuItem == 'Booking' ? '/' : `/${menuItem}`}>{menuItem}</Link>
+                    <Link href={`#${menuItem}`}>{menuItem}</Link>
                 </div>))}
 
             </div>
--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 'use client'
-import { Card, CardBody, Image } from "@nextui-org/react";
 import { BookSection } from "./Support/Componets/Bookings/BookSection";
 import { createArray } from "./Support/MyCodes/Util";
 import { Carousel, Modal } from "antd";
 import EmblaCarousel from "./Support/Componets/Carousel/Carousel";
 import { useState } from "react";
 import { ArrowLeftCircle, ArrowRightCircle, Heading1, MoveLeftIcon, MoveRightIcon } from "lucide-react";
-import { Button } from "@nextui-org/react"
 import InfoSection from "./Support/Componets/Bookings/InfoSection";
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
 
 const imgs = ['https://plus.unsplash.com/premium_photo-1700391373027-e0ba6c3da990?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1698862341365-cf4478f12332?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -28,12 +27,25 @@ const Rentals = {
 
 
 
+
 export default function Home() {
   const [openBookItem, setOpenBookItem] = useState(false)
   const [startBooking, setStartBooking] = useState(false)
   const toggleStartBooking = () => setStartBooking(!startBooking)
   return (
     <main className="min-h-screen w-full  relative  h-screen trans  bg-white">
+
+      <div className="between h-screen  relative overflow-hidden">
+        <div className="h-screen w-screen absolute top-0 center">
+          <h1 className="md:text-[200px] text-[100px] text-white  stroke-rose-800 stroke-2 drop-shadow-[0_5.2px_1.2px_rgba(0,0,0,0.8)] px-5 m-auto z-50 font-extrabold">The Hub</h1>
+
+
+        </div>
+        <div className=" overflow-hidden h-full w-full">
+          <Image className="h-screen  w-screen object-cover" src="https://images.unsplash.com/photo-1517457373958-b7bdd4587205?q=80&w=2369&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+        </div>
+      </div>
 
       {openBookItem && <div className="center">
         <Modal
@@ -56,7 +68,7 @@ export default function Home() {
       </div>}
 
 
-      <div className=" gap-4 mt-10 relative hidescroll  w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <div id="Booking" className=" gap-4 mt-10 relative hidescroll py-20 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
         {createArray(4).map(i => {
           const [currentImg, setCurrentImg] = useState(0)
           return (
@@ -86,13 +98,100 @@ export default function Home() {
 
           )
         })}
-
-
-
-
       </div>
 
 
+      <div className="h-72 bg-black">
+        <video src={'https://v.ftcdn.net/04/84/34/49/700_F_484344958_ko8I1Rh9sEHv2KVkNLg1vLK1IlxuaOIU_ST.mp4'} className="w-full aspect-video h-full object-cover" loop autoplay="true" />
+      </div>
+
+
+
+      <div id="Blog" className="max-w-full bg-black mt-8 py-20 gap-2 grid grid-cols-12 grid-rows-2 px-8">
+        <Card className="col-span-12 rounded-3xl sm:col-span-4 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col px-4 !items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
+            <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1702234893452-52302797f873?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </Card>
+        <Card className="col-span-12 rounded-3xl sm:col-span-4 h-[300px]">
+          <CardHeader className="absolute px-4 z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
+            <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://plus.unsplash.com/premium_photo-1687710306899-10a3bfcacf9b?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </Card>
+        <Card className="col-span-12 rounded-3xl sm:col-span-4 h-[300px]">
+          <CardHeader className="absolute px-4 z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
+            <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1702221422565-60f734cd90b1?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </Card>
+        <Card isFooterBlurred className="w-full h-[300px] rounded-3xl col-span-12 sm:col-span-5">
+          <CardHeader className="absolute z-10 px-4 top-1 flex-col items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">New</p>
+            <h4 className="text-black font-medium text-2xl">Acme camera</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card example background"
+            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+            src="https://images.unsplash.com/photo-1701986789743-62603f3cc027?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <CardFooter className="absolute px-4 bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+            <div>
+              <p className="text-black text-tiny">Available soon.</p>
+              <p className="text-black text-tiny">Get notified.</p>
+            </div>
+            <Button className="text-tiny" color="primary" radius="full" size="sm">
+              Notify Me
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card isFooterBlurred className="w-full rounded-3xl h-[300px] col-span-12 sm:col-span-7">
+          <CardHeader className="absolute z-10 top-1 px-4 flex-col items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
+            <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Relaxing app background"
+            className="z-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1702055894943-1f4012fbd991?q=80&w=3333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <CardFooter className="absolute px-4 bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+            <div className="flex flex-grow gap-2 items-center">
+              <Image
+                alt="Breathing app icon"
+                className="rounded-full w-10 h-11 bg-black"
+                src="https://images.unsplash.com/photo-1702221422565-60f734cd90b1?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              />
+              <div className="flex flex-col">
+                <p className="text-tiny text-white/60">Breathing App</p>
+                <p className="text-tiny text-white/60">Get a good night's sleep.</p>
+              </div>
+            </div>
+            <Button radius="full" size="sm">Get App</Button>
+          </CardFooter>
+        </Card>
+      </div>
 
 
 
